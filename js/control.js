9c16a5cbc4899dc123ec31e6759244a10d90243c
yenlinh55 <140691784+yenlinh55@users.noreply.github.com>
2023-08-14 00:59:36 +0700

    Add files via upload

---
+++ js/control.js
@@ -0,0 +1,56 @@
+$('#sp-men ').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})// JavaScript Document
+
+$('#sp-women ').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})// JavaScript Document
+
+$('#sp-kid ').owlCarousel({
+    loop:true,
+    autoplay:true,
+    margin:10,
+    nav:true,
+	dots:false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:2
+        },
+        1000:{
+            items:3
+        }
+    }
+})// JavaScript Document
